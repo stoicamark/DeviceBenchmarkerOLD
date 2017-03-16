f62fab297e557c644b7538bb6c7f7ab257adcdd7
stoicamark <stoica.mark@yahoo.com>
2017-03-16 10:28:36 +0200

    First steps

---
+++ src/ScoreHelper.ts
@@ -0,0 +1,14 @@
+/*
+* This is a helper class that contains static variables, which are used by the BenchMarker
+* to estimate a score of a device at a given time. Each variable is a coefficient.
+* The value of a coefficient describes the impact in the final score.
+* */
+export class ScoreHelper{
+    static batteryCoeff : number = 25;
+    static memoryCoeff : number = 10;
+    static isPluggedInCoeff : number = 40;
+    static wifiCoeff : number = 40;
+    static mobileDataCoeff : number = -20;
+    static cpuCoeff : number = 20;
+    static bandwidthCoeff : number = 30;
+}
---
+++ test/main.ts
@@ -0,0 +1,36 @@
+import {BenchMarker, BenchMarkerListener} from "../src/BenchMarker";
+
+class MyBenchMarkerListener implements BenchMarkerListener{
+    onBatteryStatusChanged(battery){
+        document.querySelector('#b_ischarging').textContent = (battery.charging ? 'charging' : 'not charging');
+        document.querySelector('#b_level').textContent = (Math.round(battery.level * 10000) / 100) + '%';
+        if (!battery.charging) {
+            document.querySelector('#b_discharging_time').textContent = 'Time remaining: ' + (battery.dischargingTime === Infinity ? 'Infinity' : (Math.round(100 * battery.dischargingTime / 3600) / 100) + 'h');
+        } else {
+            document.querySelector('#b_discharging_time').textContent = 'Charging Time: ' + (battery.chargingTime === Infinity ? 'Infinity' : (Math.round(100 * battery.chargingTime / 3600) / 100) + 'h');
+        }
+    }
+}
+
+let test = function(){
+    let benchMarker = BenchMarker.create(new MyBenchMarkerListener());
+    benchMarker.fetchBatteryData();
+
+    //document.querySelector('#isMobile').textContent = benchMarker.isMobile();
+/*
+    let usrAgent = window.navigator.userAgent;
+    usrAgent = usrAgent.match(/\((.*?)\)/g)[0];
+
+    let deviceInfo = usrAgent.split(';')[2];
+    deviceInfo = deviceInfo.substr(0, deviceInfo.length-1);
+
+    let osInfo = usrAgent.split(';')[1];
+
+    document.querySelector('#deviceInfo').textContent = deviceInfo;
+    document.querySelector('#osInfo').textContent = osInfo;*/
+
+    document.querySelector('#connectionType').textContent = navigator.connection.type;
+
+};
+
+test();
